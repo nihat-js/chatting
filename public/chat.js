11a89450b49192d8bc960b472a9b5b909afd6dd7
nihatxss <abdullazadenihat@gmail.com>
2021-07-15 13:02:04 -0700

    Initial commit

---
+++ public/chat.js
@@ -0,0 +1,23 @@
+const socket =io.connect('http://localhost:3000');
+
+const sender =document.getElementById('sender');
+
+const message = document.getElementById('message');
+
+const submitBtn =document.getElementById('submitBtn');
+
+const feedback =document.getElementById('feedback');
+
+const output=document.getElementById('output')
+submitBtn.addEventListener('click',()=>{
+        
+    socket.emit('chat',{
+        message:message.value ,
+        sender:sender.value
+    })
+
+})
+
+socket.on('chat',data=>{
+    output.innerHTML +='<p><strong>' +data.sender + ': </strong>' +data.message + '</p>';
+})
